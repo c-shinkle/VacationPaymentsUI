602b3ab8c950fd2e5f2b49fbbcfef2c6418cc197
Christian Shinkle <cshinkle2018@gmail.com>
2022-01-17 19:29:52 -0600

    fixed cors for local testing

--- src/api/post-data.js
+++ src/api/post-data.js
@@ -1,6 +1,8 @@
 const postData = async (url = "", data = {}) => {
   const response = await fetch(url, {
     method: "POST",
+    credentials: 'same-origin',
+    mode: 'cors',
     headers: {
       "Content-Type": "application/json",
     },
